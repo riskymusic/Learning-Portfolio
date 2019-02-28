b0dac8e58f00251d778a176166cfd1cad16d89db
Tama Owens <30021963@student.toiohomai.ac.nz>
2019-02-28 13:50:22 +1300

    https://github.com/riskymusic/Learning-Portfolio.git

---
+++ Hi how are you, where do you come from, thanks for you input.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        public static int Country { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, How old are you: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Age: " + age);
+
+
+
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("Where do you come from: ");
+            string name = (Console.ReadLine());
+            Console.WriteLine("City: " + name);
+       
+            Console.WriteLine("\nThanks for your input, we will get back to you shortly.", Country);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Name - Gender - Age - Address.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Your First Name:" +
+                "\n");
+            string name = (Console.ReadLine());
+         
+            Console.WriteLine("\nPlease Enter Your Last Name:" +
+                "\n");
+            string Name = (Console.ReadLine());
+            
+            Console.WriteLine("\nhello {0}, nice to meet you" +
+                "\n", name + " " + Name);
+
+            Console.WriteLine("What Gender are you:" + "\n");
+            string gender = Console.ReadLine();
+
+            Console.WriteLine("\nYou are " + gender);
+          
+
+            Console.WriteLine("\nwhat is your age:" + "\n");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nYou are:" + age);
+
+            Console.WriteLine("\nwhat is your address:" + "\n");
+            string address = Console.ReadLine();
+
+            Console.WriteLine("\nYou live at the address of:" + address);
+            
+
+            Console.ReadLine();
+
+            
+
+
+            
+        }
+    }
+}
